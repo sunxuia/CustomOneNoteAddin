a7f8fa2e5b013cf53ca51b9d06f799314ce999e1
sunxu <ntsunxu@163.com>
2018-10-12 18:24:37 +0800

    安装运行OK

---
+++ OneNoteAddin/Ribbon.cs
@@ -0,0 +1,81 @@
+﻿using Extensibility;
+using Microsoft.Office.Core;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+using OneNote = Microsoft.Office.Interop.OneNote;
+
+namespace OneNoteAddin
+{
+    [Guid("7C5FA097-B7EF-4B5B-93D0-8F5C71070876"), ProgId("OneNoteAddin")]
+    public class Ribbon : IDTExtensibility2, IRibbonExtensibility
+    {
+        private OneNote.Application app;
+        
+        public void OnConnection(object app, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            this.app = app as OneNote.Application;
+        }
+
+        public void OnDisconnection(ext_DisconnectMode removeMode, ref Array custom)
+        {
+            Close();
+        }
+
+        private void Close()
+        {
+            if (app != null)
+            {
+                app = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+
+        }
+
+        public void OnStartupComplete(ref Array custom)
+        {
+
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+            Close();
+        }
+        
+        public string GetCustomUI(string ribbonID)
+        {
+            return Properties.Resources.Ribbon;
+        }
+
+        public IStream GetImage(string imageName)
+        {
+            MemoryStream imageStream = new MemoryStream();
+            BindingFlags flags = BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            var prop = typeof(Properties.Resources).GetProperty(imageName ?? "", flags);
+            if (prop != null)
+            {
+                (prop.GetValue(null, null) as Image).Save(imageStream, ImageFormat.Png);
+            }
+            else
+            {
+                Properties.Resources.DefaultImage.Save(imageStream, ImageFormat.Png);
+            }
+            return new CCOMStreamWrapper(imageStream);
+        }
+
+        public void ShowHello(IRibbonControl control)
+        {
+            MessageBox.Show("hello world");
+        }
+    }
+}
